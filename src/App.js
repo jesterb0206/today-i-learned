09b59fa5eca1ec19258f619211f4b74c417e5868
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 15:21:02 -0800

    Meeting JSX and Creating the App Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import './style.css';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
