b8709828b93b7ba909882f39aa56da21c2461c22
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 01:56:01 -0800

    Creating DOM Elements

--- script.js
+++ script.js
@@ -1,8 +1,81 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: 'React is being developed by Meta (formerly facebook)',
+    source: 'https://opensource.fb.com/',
+    category: 'technology',
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
+    source:
+      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
+    category: 'society',
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: 'Lisbon is the capital of Portugal',
+    source: 'https://en.wikipedia.org/wiki/Lisbon',
+    category: 'society',
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const CATEGORIES = [
+  {name: 'technology', color: '#3b82f6'},
+  {name: 'science', color: '#16a34a'},
+  {name: 'finance', color: '#ef4444'},
+  {name: 'society', color: '#eab308'},
+  {name: 'entertainment', color: '#db2777'},
+  {name: 'health', color: '#14b8a6'},
+  {name: 'history', color: '#f97316'},
+  {name: 'news', color: '#8b5cf6'},
+];
+
+// SELECTING DOM ELEMENTS
+
 const btn = document.querySelector('.btn-open');
 const form = document.querySelector('.fact-form');
 const factsList = document.querySelector('.facts-list');
 
+// CREATE DOM ELEMENTS; RENDER FACTS IN LIST
+
 factsList.innerHTML = '';
+createFactsList(initialFacts);
+
+function createFactsList(dataArray) {
+  // factsList.insertAdjacentHTML('afterbegin', '<li>Bradley</li>');
+
+  const htmlArr = dataArray.map(
+    (fact) => `<li class="fact">
+      <p>
+      ${fact.text}
+        <a
+          class="source"
+          href="${fact.source}"
+          target="_blank"
+          >(Source)</a>
+      </p>
+      <span class="tag" style="background-color: #3b82f6">${fact.category}<span>
+    </li>`
+  );
+}
+console.log(htmlArr);
+const html = htmlArr.join('');
+factsList.insertAdjacentHTML('afterbegin', html);
+
+// TOGGLE FORM VISIBILITY
 
 btn.addEventListener('click', function () {
   if (form.classList.contains('hidden')) {
