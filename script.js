00da94634d21e3fb1b7282d7b62a57b6bb1e6fb1
jesterb0206 <jesterb@seattleu.edu>
2023-01-16 23:48:01 -0800

    Functions

--- script.js
+++ script.js
@@ -27,3 +27,16 @@ console.log('Upvotes:', totalUpvotes);
 let votesFalse = 4;
 const isCorrect = votesFalse < totalUpvotes;
 console.log(isCorrect);
+
+console.log(parseInt('25.555bbb'));
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
