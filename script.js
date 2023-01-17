956217612dd2a70fd2c38a214ca18ee4404ce811
jesterb0206 <jesterb@seattleu.edu>
2023-01-16 23:22:03 -0800

    Displaying and Hiding the Form

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+const btn = document.querySelector('.btn-open');
+const form = document.querySelector('.fact-form');
+
+btn.addEventListener('click', function () {
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    btn.textContent = 'Close';
+  } else {
+    form.classList.add('hidden');
+    btn.textContent = 'Share a fact';
+  }
+});
