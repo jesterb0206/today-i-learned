4f2226d9da2a6926ff4db1d60ee23877d136c92f
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 01:18:02 -0800

    Looping Over Arrays: The forEach and map Methods

--- script.js
+++ script.js
@@ -94,14 +94,11 @@ console.log(str);
 */
 
 const fact = ['Lisbon is the capital of Portugal', 2015, true];
-console.log(fact);
 console.log(fact[2]);
 console.log(fact.length);
 console.log(fact[fact.length - 1]);
 
 const [text, createdIn] = fact;
-console.log(createdIn);
-
 const newFact = [...fact, 'society'];
 console.log(newFact);
 
@@ -123,3 +120,74 @@ console.log(factObj['text']);
 const {category, isCorrect} = factObj;
 console.log(category);
 console.log(factObj.createSummary());
+
+// [2, 4, 6, 8].forEach(function (el) {
+//   console.log(el);
+// });
+
+// const times10 = [2, 4, 6, 8].map(function (el) {
+//   return el * 10;
+// });
+const times10 = [2, 4, 6, 8].map((el) => el * 10);
+console.log(times10);
+
+const CATEGORIES = [
+  {name: 'technology', color: '#3b82f6'},
+  {name: 'science', color: '#16a34a'},
+  {name: 'finance', color: '#ef4444'},
+  {name: 'society', color: '#eab308'},
+  {name: 'entertainment', color: '#db2777'},
+  {name: 'health', color: '#14b8a6'},
+  {name: 'history', color: '#f97316'},
+  {name: 'news', color: '#8b5cf6'},
+];
+
+const allCategories = CATEGORIES.map((el) => el.name);
+console.log(allCategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: 'React is being developed by Meta (formerly facebook)',
+    source: 'https://opensource.fb.com/',
+    category: 'technology',
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: 'Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%',
+    source:
+      'https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids',
+    category: 'society',
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: 'Lisbon is the capital of Portugal',
+    source: 'https://en.wikipedia.org/wiki/Lisbon',
+    category: 'society',
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else
+    return `Impossible year. The year needs to be less than or equal to ${currentYear}.`;
+}
+
+const factAges = initialFacts.map((el) => calcFactAge(el.createdIn));
+console.log(factAges);
+console.log(factAges.join('-'));
