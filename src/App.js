1510ccb5c1e52d321e1635c15f11b5facc10c333
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 15:43:01 -0800

    Dividing Our Interface Into Components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import './style.css';
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
+  const appTitle = 'Today I Learned';
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
