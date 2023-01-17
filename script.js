b922beca800d1da24b66cc17ec059071fbe335c5
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 01:01:04 -0800

    Objects

--- script.js
+++ script.js
@@ -104,3 +104,22 @@ console.log(createdIn);
 
 const newFact = [...fact, 'society'];
 console.log(newFact);
+
+const factObj = {
+  text: 'Lisbon is the capital of Portugal',
+  category: 'society',
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact "${
+      this.text
+    }" is from the category ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj['text']);
+
+const {category, isCorrect} = factObj;
+console.log(category);
+console.log(factObj.createSummary());
