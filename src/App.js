b0f5ef1f7f4a9a06ce09fbbbe0d8a4eab29aa959
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 18:38:02 -0800

    Working With Forms the React Way

--- src/App.js
+++ src/App.js
@@ -72,10 +72,6 @@ function Header({showForm, setShowForm}) {
   );
 }
 
-function NewFactForm() {
-  return <form className="fact-form">Fact form</form>;
-}
-
 const CATEGORIES = [
   {name: 'technology', color: '#3b82f6'},
   {name: 'science', color: '#16a34a'},
@@ -86,6 +82,45 @@ const CATEGORIES = [
   {name: 'history', color: '#f97316'},
   {name: 'news', color: '#8b5cf6'},
 ];
+
+function NewFactForm() {
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        value={source}
+        type="text"
+        placeholder="Trustworthy source..."
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter() {
   return (
