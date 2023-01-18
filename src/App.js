33c09f93e773a4a7b395f044633f9a6c5b4d4262
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 18:56:04 -0800

    Adding a New Fact - Part 1

--- src/App.js
+++ src/App.js
@@ -83,15 +83,43 @@ const CATEGORIES = [
   {name: 'news', color: '#8b5cf6'},
 ];
 
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
 function NewFactForm() {
   const [text, setText] = useState('');
-  const [source, setSource] = useState('');
+  const [source, setSource] = useState('http://example.com');
   const [category, setCategory] = useState('');
   const textLength = text.length;
 
   function handleSubmit(e) {
+    // 1. Prevent browser reload
     e.preventDefault();
     console.log(text, source, category);
+    // 2. Check if data is valid. If so, create a new fact
+    if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 10000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+      // 4. Add the new fact to the UI: add the fact to state
+      // 5. Reset input fields
+      // 6. Close the form
+    }
   }
 
   return (
