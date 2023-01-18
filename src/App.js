8da5586b43641570d1e125026881267daecbab01
jesterb0206 <jesterb@seattleu.edu>
2023-01-17 22:22:01 -0800

    Handling Votes and Updating a Fact on Supabase

--- src/App.js
+++ src/App.js
@@ -77,7 +77,11 @@ function App() {
 
       <main className="main">
         <CategoryFilter setCurrentCategory={setCurrentCategory} />
-        {isLoading ? <Loader /> : <FactList facts={facts} />}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <FactList facts={facts} setFacts={setFacts} />
+        )}
       </main>
     </>
   );
@@ -149,7 +153,7 @@ function NewFactForm({setFacts, setShowForm}) {
         .select();
       setIsUploading(false);
       // 4. Add the new fact to the UI: add the fact to state
-      setFacts((facts) => [newFact[0], ...facts]);
+      if (!error) setFacts((facts) => [newFact[0], ...facts]);
       // 5. Reset input fields
       setText('');
       setSource('');
@@ -219,7 +223,7 @@ function CategoryFilter({setCurrentCategory}) {
   );
 }
 
-function FactList({facts}) {
+function FactList({facts, setFacts}) {
   if (facts.length === 0)
     return (
       <p className="message">
@@ -232,7 +236,7 @@ function FactList({facts}) {
     <section>
       <ul className="facts-list">
         {facts.map((fact) => (
-          <Fact key={fact.id} fact={fact} />
+          <Fact key={fact.id} fact={fact} setFacts={setFacts} />
         ))}
       </ul>
       <p>There are {facts.length} facts in the database. Add your own! </p>
@@ -240,7 +244,26 @@ function FactList({facts}) {
   );
 }
 
-function Fact({fact}) {
+function Fact({fact, setFacts}) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
+
+  async function handleVote(columnName) {
+    setIsUpdating(true);
+    const {data: updatedFact, error} = await supabase
+      .from('facts')
+      .update({[columnName]: fact[columnName] + 1})
+      .eq('id', fact.id)
+      .select();
+    setIsUpdating(false);
+
+    if (!error)
+      setFacts((facts) =>
+        facts.map((f) => (f.id === fact.id ? updatedFact[0] : f))
+      );
+  }
+
   return (
     <li className="fact">
       <p>
@@ -264,9 +287,15 @@ function Fact({fact}) {
         {fact.category}
       </span>
       <div className="vote-buttons">
-        <button>👍 {fact.votesInteresting}</button>
-        <button>🤯 {fact.votesMindblowing}</button>
-        <button>⛔️ {fact.votesFalse}</button>
+        <button
+          onClick={() => handleVote('votesInteresting')}
+          disabled={isUpdating}
+        >
+          👍 {fact.votesInteresting}
+        </button>
+        <button onClick={() => handleVote('votesFalse')} disabled={isUpdating}>
+          ⛔️ {fact.votesFalse}
+        </button>
       </div>
     </li>
   );
